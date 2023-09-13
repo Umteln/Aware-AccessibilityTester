e9521d62020b24798709c7f4903b0840b1c148b6
Amera <amerawhite54@gmail.com>
2023-09-12 20:12:02 -0500

    api

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    define: {
+        __VITE_ACCESSIBILITY_API_KEY__: `"${
+            import.meta.env.VITE_ACCESSIBILITY_API_KEY
+        }"`,
+    },
+});
