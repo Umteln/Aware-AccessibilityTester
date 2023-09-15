aa8c686b46f35ce68f4dbddc192ab9e9033f78cc
Amera <amerawhite54@gmail.com>
2023-09-15 06:08:03 -0500

    api error

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    define: {
-        __API_KEY__: JSON.stringify(import.meta.env.VITE_ACCESSIBILITY_API_KEY),
-    },
 });
